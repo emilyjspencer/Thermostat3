0704f2097f69bad4bf4419751d2fc93738280547
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-22 18:06:22 +0000

    energy usage css color change halfway through

--- src/interface.js
+++ src/interface.js
@@ -1,37 +1,42 @@
 $(document).ready(function() {
-  var thermostat = new Thermostat();
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  $('#plus').on('click', function() {
-    thermostat.increase();
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  });
+    var thermostat = new Thermostat();
+    updateTemperature();
+    $("#plus").on("click", function() {
+        thermostat.increase();
+        updateTemperature();
+    });
 
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  $('#minus').on('click', function() { 
-    thermostat.decrease();
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  });
+    $("#current-temp-display").text(thermostat.currentTemperature);
+    $("#minus").on("click", function() {
+        thermostat.decrease();
+        updateTemperature();
+    });
 
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  $('#reset').on('click', function() {
-    thermostat.reset();
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  });
+    $("#current-temp-display").text(thermostat.currentTemperature);
+    $("#reset").on("click", function() {
+        thermostat.reset();
+        updateTemperature();
+    });
 
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  $('#powersaving-on').on('click', function() {
-    thermostat.switchPowerSavingModeOn();
-    $('#power-saving-status').text('on')
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  });
+    $("#current-temp-display").text(thermostat.currentTemperature);
+    $("#powersaving-on").on("click", function() {
+        thermostat.switchPowerSavingModeOn();
+        $("#power-saving-status").text("on");
+        updateTemperature();
+    });
 
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  $('#powersaving-off').on('click', function() {
-    thermostat.switchPowerSavingModeOff();
-    $('#power-saving-status').text('off')
-  $('#current-temp-display').text(thermostat.currentTemperature);
-  });
+    $("#current-temp-display").text(thermostat.currentTemperature);
+    $("#powersaving-off").on("click", function() {
+        thermostat.switchPowerSavingModeOff();
+        $("#power-saving-status").text("off");
+        updateTemperature();
+    });
+
+    function updateTemperature() {
+        $("#current-temp-display").text(thermostat.currentTemperature);
+        $("#current-temp-display").attr("class", thermostat.currentEnergyUsage());
+        if (thermostat.currentEnergyUsage() === "low") {
+            $("current-temp-display").css("color", "green");
+        }
+    }
 });
-
-
-
